3719f752d88e5d15518061e219d2f542cac70c5c
breetisalwayshungry <164559413+breetisalwayshungry@users.noreply.github.com>
2025-04-06 16:45:35 -0400

    Update transceiver.py

--- transceiver.py
+++ transceiver.py
@@ -19,37 +19,29 @@ def setup_radio():
 	radio.startListening()
 
 def send_telemetry(message="test"): # default 'test' if no message passed
-	message += "\n"
-
-	chunk_size = 32 # size limit of each package
-	message_length = len(message)
-
 	radio.stopListening()
 
-	# break message into chunks and send
-	for i in range(0, message_length, chunk_size): 
-		chunk = message[i:i + chunk_size]
-		success = radio.write(chunk.encode('utf-8'))
-		if success: 
-			print(f"Sent: {chunk}")
-		time.sleep(0.1)
+	success = radio.write(chunk.encode('utf-8'))
+	if success: 
+		print(f"[transceiver.py] Sent: {chunk}")
+	time.sleep(0.1)
 
 	radio.startListening()
 
 def receive_command(): 
 	if radio.available(): 
 		command = radio.read(radio.getDynamicPayloadSize()).decode('utf-8')
-		print(f"Command received: {command}")
+		print(f"[transceiver.py] Command received: {command}")
 		process_command(command)
 
 def process_command(command):
 	if command == "TEST": 
-		print("Test Successful")
+		print("[transceiver.py] Test Successful")
 	elif command == "example": 
 		# perform action
-		print("This is an example")
+		print("[transceiver.py] this is an example")
 	else: 
-		print(f"Unknown command: {command}")
+		print(f"[transceiver.py] Unknown command: {command}")
 
 if __name__ == "__main__":
 	setup_radio()
